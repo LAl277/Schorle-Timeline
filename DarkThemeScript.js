ef56e14a0ce784da64510d853e6f06a2d91221e8
LA <louis.lindner@web.de>
2024-03-28 22:33:05 +0100

    base

---
+++ DarkThemeScript.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.getElementById("toggle-dark-mode");
+  const currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme) {
+    document.body.classList.toggle("dark-mode", currentTheme === "dark");
+    toggleSwitch.checked = currentTheme === "dark";
+  }
+
+  toggleSwitch.addEventListener("change", switchTheme);
+
+  function switchTheme(event) {
+    if (event.target.checked) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  }
+});
